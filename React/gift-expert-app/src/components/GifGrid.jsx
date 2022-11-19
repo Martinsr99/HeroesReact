fbe8fa90e56153ebb145561c07edb7f7a881b30c
Martinsr99 <53481948+Martinsr99@users.noreply.github.com>
2022-11-19 23:12:49 +0100

    Add isLoading and END.

--- React/gift-expert-app/src/components/GifGrid.jsx
+++ React/gift-expert-app/src/components/GifGrid.jsx
@@ -1,25 +1,18 @@
-import {useState, useEffect} from 'react'
-import { getGifs } from "../helpers/getGifs"
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifItem } from './GifItem'
 
 
 
 export const GifGrid = ({category}) => {
   
-  const [images, setImages] = useState([])
-
-  useEffect( () => {
-    getGifs(category)
-      .then(newImages => setImages(newImages))
-  }, [])
-
-  getGifs(category)
-
-  const gifs = []
+  const { images, isLoading } = useFetchGifs(category)
 
   return (
     <>
         <h3>{category}</h3>
+        {
+          isLoading && ( <h2>Cargando...</h2>) 
+        }
         
         <div className="card-grid">
           {
