f158f7c2c427d1bc27af95fef254c335eb1dc066
Martinsr99 <53481948+Martinsr99@users.noreply.github.com>
2022-11-19 20:33:03 +0100

    Add functionality to retrieve images and display title

--- React/gift-expert-app/src/components/GifGrid.jsx
+++ React/gift-expert-app/src/components/GifGrid.jsx
@@ -1,9 +1,30 @@
+import {useState, useEffect} from 'react'
+import { getGifs } from "../helpers/getGifs"
+
 
 
 export const GifGrid = ({category}) => {
+  
+  const [images, setImages] = useState([])
+
+  useEffect( () => {
+    getGifs(category)
+      .then(newImages => setImages(newImages))
+  }, [])
+
+  getGifs(category)
+
+  const gifs = []
+
   return (
     <>
         <h3>{category}</h3>
+        
+        <ol>
+          {
+            images.map(image => (<li key={image.id}>{image.title}</li>))
+          }
+        </ol>
     </>
   )
 }
