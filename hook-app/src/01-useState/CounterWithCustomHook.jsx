350525fd1644c4c7325a028dd60d871a19c29cd5
Martinsr99 <53481948+Martinsr99@users.noreply.github.com>
2022-11-20 17:13:41 +0100

    Create HooksApp and implement  useState

---
+++ hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+export const CounterWithCustomHook = () => {
+  return (
+    <>
+        <h1>Counter with Hook: XX</h1>
+        <hr />
+
+
+        <button className='btn btn-primary'>+1</button>
+        <button className='btn btn-primary'>Reset</button>
+        <button className='btn btn-primary'>-1</button>
+    
+    
+    
+    
+    </>
+  )
+}
