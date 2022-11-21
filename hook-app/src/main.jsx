4f0405d46827b0a0982328e7285c8e0bd7d7dc7e
Martinsr99 <53481948+Martinsr99@users.noreply.github.com>
2022-11-21 18:26:29 +0100

    END useLayoutEffect

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { FocusScreen } from './04-useRef/focusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
+// import { FocusScreen } from './04-useRef/focusScreen'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -12,6 +13,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Layout />
   </React.StrictMode>
 )
