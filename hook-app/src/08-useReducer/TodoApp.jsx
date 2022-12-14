f19f701ea395d0ebcb7eea326e07258a853eadc2
Martinsr99 <53481948+Martinsr99@users.noreply.github.com>
2022-12-14 19:45:51 +0100

    Delete TODO and mark as completed

--- hook-app/src/08-useReducer/TodoApp.jsx
+++ hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,33 +1,50 @@
+import { useEffect } from "react";
 import { useReducer } from "react";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
 import { todoReducer } from "./todoReducer";
 
 const initialState = [
-  {
-    id: new Date().getTime(),
-    description: "Recolectar la piedra del alma",
-    done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    description: "Recolectar la piedra del tiempo",
-    done: false,
-  },
+  // {
+  //   id: new Date().getTime(),
+  //   description: "Recolectar la piedra del alma",
+  //   done: false,
+  // }
 ];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
 
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const handleNewTodo = (todo) => {
     const action = {
-      type: '[TODO] Add Todo',
-      payload: todo
-    }
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
 
-    dispatch(action)
-  }
+    dispatch(action);
+  };
 
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Delete Todo",
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
   return (
     <>
       <h1>
@@ -37,13 +54,17 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-            <TodoList todos={todos}/>
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
 
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
-            <TodoAdd onNewTodo={handleNewTodo}/>
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
     </>
