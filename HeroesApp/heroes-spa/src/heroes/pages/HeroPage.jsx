25029e4410fe16bdadaf68c7d7651c8fa5cc819c
Martinsr99 <silesreche.martin@gmail.com>
2023-02-01 17:57:15 +0100

    Add search functionality and animations to images

--- HeroesApp/heroes-spa/src/heroes/pages/HeroPage.jsx
+++ HeroesApp/heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,42 @@
+import { useMemo } from "react";
+import { Navigate, useParams,useNavigate } from "react-router-dom";
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+  const { id, ...rest } = useParams();
+
+  const hero = useMemo(() => getHeroById(id),[id]); 
+  const heroURL = `/assets/${id}.jpg`;
+
+  if (!hero) {
+    return <Navigate to="/marvel"></Navigate>;
+  }
+
+  const navigate = useNavigate();
+
   return (
-    <h1>HeroPage</h1>
-  )
-}
+    <>
+      <div className="row mt-5">
+        <div className="col-4">
+          <img src={heroURL} className="img-thumbnail animate__animated animate__fadeInLeft" alt={hero.superhero} />
+        </div>
+        <div className="col-8">
+          <h3>{hero.superhero}</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"><b>Alter ego</b> {hero.alter_ego}</li>
+            <li className="list-group-item"><b>Publisher</b> {hero.publisher}</li>
+            <li className="list-group-item"><b>First appearance</b> {hero.first_appearance}</li>
+          </ul>
+
+          <h5 className="mt-3">Characters</h5>
+          <p>{hero.characters}</p>
+
+          <button 
+          className="btn btn-outline-primary"
+          onClick={() => navigate(-1)}
+          >Back</button>
+        </div>
+      </div>
+    </>
+  );
+};
