f20fc1b9d88dad8df1542144225d844b18b5cc0a
Martinsr99 <silesreche.martin@gmail.com>
2023-02-03 23:36:20 +0100

    Improve public routes and add functionality to get back to last page

--- HeroesApp/heroes-spa/src/auth/pages/LoginPage.jsx
+++ HeroesApp/heroes-spa/src/auth/pages/LoginPage.jsx
@@ -10,9 +10,11 @@ export const LoginPage = () => {
 
   const onLogin = () => {
 
+    const lastPath = localStorage.getItem('lastPath') || '/'
+
     login('Martin Siles')
 
-    navigate('/', {
+    navigate(lastPath, {
       replace: true
   });
   }
